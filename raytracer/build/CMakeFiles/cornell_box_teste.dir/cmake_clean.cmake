1be8c400d1b762e620c9329b699cc99d6e25d57a
Matheus Oliveira Silva <matheussilva@tecgraf.puc-rio.br>
2025-04-02 08:17:41 -0300

    Auto stash before checking out "HEAD"

---
+++ raytracer/build/CMakeFiles/cornell_box_teste.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cornell_box_teste.dir/examples/cornell_box_teste.cpp.o"
+  "CMakeFiles/cornell_box_teste.dir/examples/cornell_box_teste.cpp.o.d"
+  "bin/cornell_box_teste"
+  "bin/cornell_box_teste.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cornell_box_teste.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ raytracer/build/CMakeFiles/cornell_box_teste.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for cornell_box_teste.
+# This may be replaced when dependencies are built.
